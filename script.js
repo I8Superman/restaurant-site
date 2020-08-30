431d8b5ecb1a59d880edac4f6155939b001e03bf
Simon Andersen <simonandersen@Simons-MacBook-Air-2.local>
2020-08-30 15:06:39 +0200

    Template and cloning test completed

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // js here
+
+const template = document.querySelector("#templatetest").content;
+
+console.log("template created");
+
+const cloneTest = template.cloneNode(true);
+
+cloneTest.querySelector("h3").textContent = "A NICE Russian meal!";
+cloneTest.querySelector("p").textContent = "A completely different short description";
+cloneTest.querySelector("h4").textContent = "New price: 2 dollars!";
+
+console.log("content changed");
+
+const whosyourDaddy = document.querySelector("#starters");
+
+whosyourDaddy.appendChild(cloneTest);
+
+console.log("appended to parent element!");
+
+/*
+
+//Step 4: Chose the new "parent" element
+const parentElement = document.querySelector("body");
+
+//Step 5: Add (Append) the clone to the DOM
+parentElement.appendChild(myClone);*/
