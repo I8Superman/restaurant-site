ddb2162701e096a24ef9647a46840815130987be
Simon Andersen <simonandersen@laptop-ts8apqp1.kea.local>
2020-09-01 10:58:42 +0200

    Section categories created

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-// fetch data
+// Fetch the data - categories
 
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-    .then(function (res) {
-        return res.json();
-        console.log(res)
+init();
+
+function init() {
+    fetch("https://kea-alt-del.dk/t5/api/categories").then(response => response.json()).then(
+        function (data) {
+            categoriesReceived(data)
+        })
+}
+
+function categoriesReceived(cats) {
+    console.log(cats);
+    createSections(cats);
+}
+
+function createSections(categories) {
+categories.forEach(category => {
+    const section = document.createElement("section");
+    section.setAttribute("id", category);
+    const h2 = document.createElement("h2");
+    h2.textContent = category;
+    section.appendChild(h2);
+    document.querySelector(".productlist").appendChild(section);
+})
+}
+
+// Fetch the data - productlist
+
+/*fetch("https://kea-alt-del.dk/t5/api/productlist")
+    .then(function (response) {
+        console.log(response);
+        return response.json();
     })
-    .then(function (data) {
-        console.log(data)
+    .then(function(data) {
+        console.log(data);
         dataReceived(data);
     })
 
-// Here we pass the data to a function andgive he data a new name - products
 
-function dateReceived(courses) {
-    // loop through data
+function dataReceived(courses) {
+// loop through data
     courses.forEach(showCourse);
 }
 
-function showCourse(myCourse) {
-console.log(mycourse)
-}
-
-// find the template
-
-// clone template
-
+// Executed once for each course
+function showCourse(thisCourse) {
+// Find the template
+const template = document.querySelector("#courseTemplate").content;
+// clone the template
+const myCopy = template.cloneNode(true);
 // Fill out the template
-
+myCopy.querySelector(".course_name").textContent = thisCourse.name;
 // Append the template
+const parentElement = document.querySelector("section#starters");
+parentElement.appendChild(myCopy);
+}
+*/
 
 
 /* Cloning example:
